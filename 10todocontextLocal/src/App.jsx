2b9dd68809a5be4058a299dc7a07a659bdc5e2e1
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-03-29 20:10:43 +0530

    add new features in todo

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos,setTodos] = useState([])
+
+  const addTodo =(todo)=>{
+      setTodos((prev)=>[{id:Date.now(),...todo} ,prev])
+  }
+
 
   return (
-    <>
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
       <h1 className='underline'>Hello</h1>
-    </>
+    </TodoProvider>
   )
 }
 
